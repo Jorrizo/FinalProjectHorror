cc66a88ddbd6a05db2a91ed0d51a924c43f7ba2f
Jorrizo <jorris.hentz@gmail.com>
2020-01-09 15:16:43 +0100

    crash Scène Jorrizo + test de raycast + 2 test materials

---
+++ Assets/Scripts/RaycastSound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastSound : MonoBehaviour
+{
+
+    public GameObject monster;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = monster.transform.position - transform.position;
+        
+        Debug.DrawRay(transform.position, direction, Color.red);
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, direction);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.tag == "wall")
+            {
+                Debug.Log("WOW UN MUR !!");
+            }
+        }
+    }
+}
