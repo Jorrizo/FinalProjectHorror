cfd7644fe6a1c089fa818773014c8c2cb582be31
sassyCorbeau <sassycorbeau@gmail.com>
2020-01-25 16:05:35 +0100

    Début de script pour se cacher

---
+++ Assets/Scripts/HidingRaycast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingRaycast : MonoBehaviour
+{
+
+    Ray ray;
+    RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            if (hit.collider.gameObject.CompareTag("Interactive"))
+            {
+                Debug.Log("Itworks");
+            }
+        
+        }
+
+
+    }
+}
