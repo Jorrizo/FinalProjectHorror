888c2e1dce3b51afc6fa53cf16cf8b8e2a11558e
Jorrizo <jorris.hentz@gmail.com>
2020-02-04 09:09:02 +0100

    L'IA mais elle a la flemme de marcher trop, je trouve un moyen de la motiver. Genre des chocolatines le matins

---
+++ Assets/Scripts/AImovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AImovement : MonoBehaviour
+{
+
+    [SerializeField] private NavMeshAgent NavMeshAgent;
+    [SerializeField] private GameObject m_Target1;
+    [SerializeField] private GameObject m_Target2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent.SetDestination(m_Target2.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //private void OnTriggerEnter(Collider : Collision)
+    //{
+        //if(Collision = tag"NavMeshAgent.SetDestination(m_Target1.transform.position);
+    //z}
+}
