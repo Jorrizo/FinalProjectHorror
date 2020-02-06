97e1b821e942d29c4f24fad95d697923c15a95c9
sassyCorbeau <sassycorbeau@gmail.com>
2020-02-06 15:53:56 +0100

    Sourcetree c'est chiant

--- Assets/Scripts/HidingRaycast.cs
+++ Assets/Scripts/HidingRaycast.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class HidingRaycast : MonoBehaviour
 {
-
+    
     Ray ray;
     RaycastHit hit;
+
+    public int RaySize = 10;
+
+    private Transform _selection;
 
     // Start is called before the first frame update
     void Start()
@@ -18,15 +22,31 @@ public class HidingRaycast : MonoBehaviour
     void Update()
     {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out hit, 100))
-        {
-            Debug.DrawLine(ray.origin, hit.point);
-            if (hit.collider.gameObject.CompareTag("Interactive"))
-            {
-                Debug.Log("Itworks");
-            }
         
+        if(_selection != null)
+        {
+            _selection.GetComponent<Outline>().enabled = false;
+            _selection = null;
+        }
+
+        if (Physics.Raycast(ray, out hit, RaySize))
+        {
+            var selection = hit.transform;
+            Debug.DrawLine(ray.origin, hit.point);
+
+            if (selection.CompareTag("Interactive"))
+            {
+                hit.collider.gameObject.GetComponent<Outline>().enabled = true;                
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Debug.Log("Itworks");
+                }
+
+                _selection = selection;              
+            }
+            
+       
         }
 
 
