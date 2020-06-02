af001e5a2aa5c774d734d37cb7513dbf1f6c46e8
Jorrizo <jorris.hentz@gmail.com>
2020-06-02 13:15:23 +0200

    le truc que ona aps ele temps

---
+++ Assets/lightOnOff.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightOnOff : MonoBehaviour
+{
+    public Light myLight;        // Your light
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        flash();
+    }
+
+    public void flash()
+    {
+        if(myLight.intensity <= 4)
+        {
+            myLight.intensity += 1 * Time.deltaTime;
+        }
+
+        if(myLight.intensity >= 7)
+        {
+            myLight.intensity -= 1 * Time.deltaTime;
+        }
+    }
+}
