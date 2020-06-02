74aa5833cd4b18b7252b3bfa60ecfeb4f1c87d86
Jorrizo <jorris.hentz@gmail.com>
2020-06-02 14:05:33 +0200

    le problème

---
+++ Assets/Scripts/WaypointPath.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPath : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public GameObject destination1;
+    public GameObject destination2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.SetDestination(destination1.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Destination1"))
+        {
+            agent.SetDestination(destination2.transform.position);
+        }
+
+        if (other.CompareTag("Destination2"))
+        {
+            agent.SetDestination(destination1.transform.position);
+        }
+    }
+}
