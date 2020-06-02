5d79b64783d961f56314df9845b33477972d0497
sassyCorbeau <sassycorbeau@gmail.com>
2020-06-02 12:45:13 +0100

    menu fin

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void ButtonQuitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
