59b0c599a00562e5794573aeb92a2b0bae2896c8
sassyCorbeau <sassycorbeau@gmail.com>
2020-06-02 15:27:18 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Assets/Scripts/Objectives.cs
+++ Assets/Scripts/Objectives.cs
@@ -6,6 +6,7 @@ public class Objectives : MonoBehaviour
 {
     public int objectiveNumber;
 
+    public GameObject objectif;
     public void Interact()
     {
         if(objectiveNumber == GameManager.objectivesDone)
@@ -14,7 +15,7 @@ public class Objectives : MonoBehaviour
             GameManager.objectivesDone++;
             Debug.Log(GameManager.objectivesDone);
             PlayerPrefs.SetInt("Objectif", GameManager.objectivesBeforeDeath += 1);
-            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            objectif.SetActive(true);
         }
         else
         {
